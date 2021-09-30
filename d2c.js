c0ec8c3db99d3280d24e89cfcd5b708136471181
Andrew Beatrice <abeatrice@consumertrack.com>
2021-09-30 12:58:18 -0700

    wip

--- d2c.js
+++ d2c.js
@@ -1,22 +1,32 @@
 (function() {
 
-  var sendImpression = function(host) {
+  var sendImpressions = function(host, impressions) {
     var xhr = new XMLHttpRequest();
     var url = host + '/t/';
     xhr.open('POST', url);
     xhr.setRequestHeader('Content-Type', 'application/json');
-    xhr.send(JSON.stringify(["/pub/i?pubid=1234","/pub/td?pubid=1234"]));
+    xhr.send(JSON.stringify(impressions));
+  }
+
+  var getParams = function(path, search) {
+    console.log(path)
+    var params = [
+      "pubid="
+    ];
+    return "/pub/i?" + params.join('&');
   }
 
   var fn = function() {
     var anchors = document.querySelectorAll('a[href*="products.gobankingrates.com"]');
+    var impressions = [];
     for (let i = 0; i < anchors.length; i++) {
-      //filter to get distinct pubp + query params
+      //TODO filter to get distinct pubp + query params
       var anchor = anchors[i];
       var url = new URL(anchor.href);
       console.log(url);
-      sendImpression(url.origin);
+      impressions.push(getParams(url.pathname, url.search));
     }
+    sendImpressions(url.origin, impressions);
   };
 
   if (document.readyState === "complete" || document.readyState === "interactive") {
