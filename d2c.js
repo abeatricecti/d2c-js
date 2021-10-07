22fe43e240e5d419f1296a5a0a558bcac4d63619
Andrew Beatrice <abeatrice@consumertrack.com>
2021-10-07 13:59:58 -0700

    min d2c.js

--- d2c.js
+++ d2c.js
@@ -1,41 +1,23 @@
 (function() {
-
-  var sendImpressions = function(url, impressions) {
+  var fn = function() {
+    var anchors = {}, impressions = [];
+    var query = document.querySelectorAll('a[href*="products.gobankingrates.com/pub/"]');
+    for (var i = 0; i < query.length; i++) {
+      var url = new URL(query[i].href);
+      var pub = url.pathname.replace('/pub/', '').split('/')[0];
+      anchors[pub + url.search] = { "pub": pub, "url": url, "search": url.search };
+    }
+    if (anchors.length < 1) return;
+    for (var i = 0; i < Object.keys(anchors).length; i++) {
+      var anchor = anchors[Object.keys(anchors)[i]];
+      impressions.push("/pub/id2c?pubid=" + anchor.pub + anchor.search.replace('?', '&'));
+    }
+    var url = anchors[Object.keys(anchors)[0]].url.origin + '/t/';
     var xhr = new XMLHttpRequest();
     xhr.open('POST', url);
     xhr.setRequestHeader('Content-Type', 'application/json');
     xhr.send(JSON.stringify(impressions));
-  }
-
-  var uniqueAnchors = function() {
-    var anchors = document.querySelectorAll('a[href*="products.gobankingrates.com/pub/"]');
-    var obj = {};
-    for (var i = 0; i < anchors.length; i++) {
-      var url = new URL(anchors[i].href);
-      var pub = url.pathname.replace('/pub/', '').split('/')[0];
-      var key = pub + url.search;
-      obj[key] = {
-        "pub": pub,
-        "url": url,
-        "search": url.search
-      };
-    }
-    return obj;
-  }
-
-  var fn = function() {
-    var anchors = uniqueAnchors();
-    if (anchors.length < 1) return;
-    var impressions = [];
-    for (var i = 0; i < Object.keys(anchors).length; i++) {
-      var anchor = anchors[Object.keys(anchors)[i]];
-      var impression = "/pub/i?pubid=" + anchor.pub + anchor.search.replace('?', '&');
-      impressions.push(impression);
-    }
-    var url = anchors[Object.keys(anchors)[0]].url.origin + '/t/';
-    sendImpressions(url, impressions);
   };
-
   if (document.readyState === "complete" || document.readyState === "interactive") {
     setTimeout(fn, 1);
   } else {
